baa9b740b786b28773a36c99597014b070e0bbc6
Prateek0406 <f2015713@pilani.bits-pilani.ac.in>
2017-06-02 13:17:17 +0530

    Added a few test cases
     For node and relationship creation

---
+++ test/gnowdb/neo4j/gdriver_test.clj
@@ -0,0 +1,249 @@
+(ns gnowdb.neo4j.gdriver_test
+  (:require [clojure.test :refer :all]
+  
+            [gnowdb.neo4j.gdriver :refer :all]
+
+  
+            )
+  )
+
+(deftest runQuery-test
+		(testing "Error in node creation:"
+			(testing "Empty Query"
+				(is (= {:results [],
+ :summary
+ {:summaryMap
+  {:relationshipsCreated 0,
+   :containsUpdates false,
+   :nodesCreated 0,
+   :nodesDeleted 0,
+   :indexesRemoved 0,
+   :labelsRemoved 0,
+   :constraintsAdded 0,
+   :propertiesSet 0,
+   :labelsAdded 0,
+   :constraintsRemoved 0,
+   :indexesAdded 0,
+   :relationshipsDeleted 0},
+  :summaryString "ContainsUpdates :false ;"}}
+ (runQuery)
+
+					)
+				)
+		    )
+
+		    (testing "no node name only parameters"
+		    	(is (= {:results [()],
+ :summary
+ {:summaryMap
+  {:relationshipsCreated 0,
+   :containsUpdates true,
+   :nodesCreated 1,
+   :nodesDeleted 0,
+   :indexesRemoved 0,
+   :labelsRemoved 0,
+   :constraintsAdded 0,
+   :propertiesSet 0,
+   :labelsAdded 0,
+   :constraintsRemoved 0,
+   :indexesAdded 0,
+   :relationshipsDeleted 0},
+  :summaryString "ContainsUpdates :true ;NodesCreated :1 ;"}}
+ (runQuery {:query "Create ()" :parameters {"a" "test_case2"}})
+		    	    )
+		    	)
+		    )
+
+		    (testing "no parameter" 
+		    	(is (= {:results [()],
+ :summary
+ {:summaryMap
+  {:relationshipsCreated 0,
+   :containsUpdates true,
+   :nodesCreated 1,
+   :nodesDeleted 0,
+   :indexesRemoved 0,
+   :labelsRemoved 0,
+   :constraintsAdded 0,
+   :propertiesSet 0,
+   :labelsAdded 0,
+   :constraintsRemoved 0,
+   :indexesAdded 0,
+   :relationshipsDeleted 0},
+  :summaryString "ContainsUpdates :true ;NodesCreated :1 ;"}}
+   (runQuery {:query "Create (test_case3)" :parameters {}}))
+
+		    	)
+		    )
+		    (testing "two nodes simultaneously"
+		    	(is (= {:results [()],
+ :summary
+ {:summaryMap
+  {:relationshipsCreated 0,
+   :containsUpdates true,
+   :nodesCreated 2,
+   :nodesDeleted 0,
+   :indexesRemoved 0,
+   :labelsRemoved 0,
+   :constraintsAdded 0,
+   :propertiesSet 0,
+   :labelsAdded 0,
+   :constraintsRemoved 0,
+   :indexesAdded 0,
+   :relationshipsDeleted 0},
+  :summaryString "ContainsUpdates :true ;NodesCreated :2 ;"}}
+ (runQuery {:query "Create (node1),(node2)" :parameters {}})
+		    	    )
+		    
+		        )
+
+		    )
+
+		    (testing "two parameters for single node"
+		    	(is (= {:results [()],
+ :summary
+ {:summaryMap
+  {:relationshipsCreated 0,
+   :containsUpdates true,
+   :nodesCreated 1,
+   :nodesDeleted 0,
+   :indexesRemoved 0,
+   :labelsRemoved 0,
+   :constraintsAdded 0,
+   :propertiesSet 2,
+   :labelsAdded 0,
+   :constraintsRemoved 0,
+   :indexesAdded 0,
+   :relationshipsDeleted 0},
+  :summaryString
+  "ContainsUpdates :true ;NodesCreated :1 ;PropertiesSet :2 ;"}} 
+  (runQuery {:query "Create (node2 {name:{a},place:{b}})" :parameters {"a" "test_case4" "b" "Mumbai"}}) 
+		    		)
+		    	
+		    	)
+
+		    )
+
+		    (testing "more than one nodes each with more than one parameter"
+		    	(is (= {:results [()],
+ :summary
+ {:summaryMap
+  {:relationshipsCreated 0,
+   :containsUpdates true,
+   :nodesCreated 2,
+   :nodesDeleted 0,
+   :indexesRemoved 0,
+   :labelsRemoved 0,
+   :constraintsAdded 0,
+   :propertiesSet 4,
+   :labelsAdded 2,
+   :constraintsRemoved 0,
+   :indexesAdded 0,
+   :relationshipsDeleted 0},
+  :summaryString
+  "ContainsUpdates :true ;NodesCreated :2 ;PropertiesSet :4 ;LabelsAdded :2 ;"}} 
+  (runQuery {:query "Create (node2:test {name:{a},place:{b}}), (node3:test {name:{c}, place:{d}})" :parameters {"a" "test_case4" "b" "Mumbai" "c" "test_case5" "d" "mumbai"}})
+		    		)
+		    	)
+		    )
+        )
+        (testing "Error in creating Relationships"
+        	
+
+        	(testing "Relationship query between two nodes"
+        		(is (= {:results [()],
+ :summary
+ {:summaryMap
+  {:relationshipsCreated 1,
+   :containsUpdates true,
+   :nodesCreated 2,
+   :nodesDeleted 0,
+   :indexesRemoved 0,
+   :labelsRemoved 0,
+   :constraintsAdded 0,
+   :propertiesSet 4,
+   :labelsAdded 2,
+   :constraintsRemoved 0,
+   :indexesAdded 0,
+   :relationshipsDeleted 0},
+  :summaryString
+  "RelationshipsCreated :1 ;ContainsUpdates :true ;NodesCreated :2 ;PropertiesSet :4 ;LabelsAdded :2 ;"}} 
+  (runQuery {:query "Create (node2:test {name:{a},place:{b}}), (node3:test {name:{c}, place:{d}}), (node2)-[:r]->(node3)" :parameters {"a" "test_case 6" "b" "Mumbai" "c" "test_case5" "d" "mumbai"}}) 
+        			) 
+        		)
+
+        	)
+
+     
+        	(testing "Two way relationship"
+        		(is (= {:results [()],
+ :summary
+ {:summaryMap
+  {:relationshipsCreated 2,
+   :containsUpdates true,
+   :nodesCreated 2,
+   :nodesDeleted 0,
+   :indexesRemoved 0,
+   :labelsRemoved 0,
+   :constraintsAdded 0,
+   :propertiesSet 4,
+   :labelsAdded 2,
+   :constraintsRemoved 0,
+   :indexesAdded 0,
+   :relationshipsDeleted 0},
+  :summaryString
+  "RelationshipsCreated :2 ;ContainsUpdates :true ;NodesCreated :2 ;PropertiesSet :4 ;LabelsAdded :2 ;"}} 
+  (runQuery {:query "Create (node2:test {name:{a},place:{b}}), (node3:test {name:{c}, place:{d}}), (node2)-[:r]->(node3), (node3)-[:r]->(node2)" :parameters {"a" "test_case 6" "b" "Mumbai" "c" "test_case5" "d" "mumbai"}}) 
+
+        			)
+        		)
+        	
+        	)
+
+        	(testing "relationship with parameters"
+        		(is (= {:results [()],
+ :summary
+ {:summaryMap
+  {:relationshipsCreated 1,
+   :containsUpdates true,
+   :nodesCreated 2,
+   :nodesDeleted 0,
+   :indexesRemoved 0,
+   :labelsRemoved 0,
+   :constraintsAdded 0,
+   :propertiesSet 6,
+   :labelsAdded 2,
+   :constraintsRemoved 0,
+   :indexesAdded 0,
+   :relationshipsDeleted 0},
+  :summaryString
+  "RelationshipsCreated :1 ;ContainsUpdates :true ;NodesCreated :2 ;PropertiesSet :6 ;LabelsAdded :2 ;"}} 
+  (runQuery {:query "Create (node2:test {name:{a},place:{b}}), (node3:test {name:{c}, place:{d}}), (node2)-[:r {name:{e}, type:{f}}]->(node3)" :parameters {"a" "test_case 6" "b" "Mumbai" "c" "test_case5" "d" "mumbai" "e" "test_rel1" "f" "test_rel2"}})
+        			)
+        		)
+        	)
+        	(testing "relationship between pre-existing nodes"
+        		(is (= 
+        			)
+        		)
+        	)
+        	(testing "creating paths"
+        		(is (=  
+        			)
+        		)
+
+        	)
+
+        )
+
+        (testing "Error in merging"
+        	(testing ""
+        	)
+
+        )
+
+
+
+)
+		    
+(run-tests)

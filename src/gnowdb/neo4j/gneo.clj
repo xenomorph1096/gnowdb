1d5b3e1e08a2a958a45bfc9f00b97c8226861501
RohitG28 <rohit.ghivdonde@gmail.com>
2017-06-20 19:30:38 +0530

    Added a few testcases for gneo.clj

--- src/gnowdb/neo4j/gneo.clj
+++ src/gnowdb/neo4j/gneo.clj
@@ -2451,6 +2451,7 @@
       )
     )
   )
+
 (defn deleteAttributeType
   "Delete an AttributeType.
   :forceMigrate?"
@@ -2778,7 +2779,7 @@
 			)
         ]
     (if (not (empty? subClassOf))
-                                        ;"Adds the attributes,NeoConstraints,CustomConstraints and ApplicableRelationNTs of the superclass to the subclass"
+    ;"Adds the attributes,NeoConstraints,CustomConstraints and ApplicableRelationNTs of the superclass to the subclass"
       (let
           [completeQueryVec (vec
                              (concat [createNewNodeQuery]
